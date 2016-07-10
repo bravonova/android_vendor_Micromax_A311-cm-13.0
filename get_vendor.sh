9f1805a69020f79353028bf8d59a64b6666ef42f
thp1997 <tirthpatel49@gmail.com>
2016-07-10 12:52:57 +0530

    Update get_vendor.sh

--- get_vendor.sh
+++ get_vendor.sh
@@ -19,7 +19,7 @@ WIFI="/etc/wifi/ /bin/6620_wmt_lpbk /bin/6620_launcher /bin/6620_wmt_concurrency
 # gralloc && hwcomposer - hardware layer. rest is userspace lib.so layer.
 #
 GL="/lib/egl/libGLESv1_CM_mali.so /lib/egl/libGLESv2_mali.so /lib/egl/libEGL_mali.so \
-/lib/libm4u.so /lib/hw/hwcomposer.mt6582.so /lib/hw/gralloc.mt6582.so \
+/lib/libm4u.so /lib/hw/hwcomposer.mt6592.so /lib/hw/gralloc.mt6592.so \
 /lib/libdpframework.so /lib/libion.so /lib/libMali.so"
 
 #
