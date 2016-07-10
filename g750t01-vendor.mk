710df7238f19d52d9e39bdd8133d81b83007e997
thp1997 <tirthpatel49@gmail.com>
2016-07-10 11:14:06 +0530

    Initial commit

---
+++ g750t01-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A311/xbin,system/xbin)
+
